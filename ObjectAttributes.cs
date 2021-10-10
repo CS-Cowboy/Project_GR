37c7286407e180e6a877cac625066e47a1a1061c
Jacob <70616107+CS-Cowboy@users.noreply.github.com>
2021-10-09 19:59:52 -0500

    Added necessary variable
    .

--- ObjectAttributes.cs
+++ ObjectAttributes.cs
@@ -9,6 +9,8 @@ namespace com.braineeeeDevs.gr
         /// We are using metric in this simulation.
         /// </summary>
         public float topSpeed, mass, drag, fireDelayTime, reloadDelayTime;
+        
+        public System.Guid poolID = System.Guid.Empty; //Important for ID persistance and pooling of objects.
         public uint poolCapacity = 5;
 
     }
