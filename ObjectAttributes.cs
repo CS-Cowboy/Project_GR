b6bbf11f40e63b857d787d245f57689e8cf37141
Jacob <70616107+CS-Cowboy@users.noreply.github.com>
2021-10-09 19:50:47 -0500

    Including update version of ObjectAttributes
    Including more appropriate structure and variables.

--- ObjectAttributes.cs
+++ ObjectAttributes.cs
@@ -9,9 +9,8 @@ namespace com.braineeeeDevs.gr
         /// <summary>
         /// We are using metric in this simulation.
         /// </summary>
-        public float topSpeed, brakingForce = 200f, headlampRange = 20f, headlampAngle = 16f, highBeamRange = 40f, highBeamSpotAngle = 40f, mass, drag, fireDelayTime, reloadDelayTime;
+        public float topSpeed, mass, drag, fireDelayTime, reloadDelayTime;
         public uint poolCapacity = 5;
-        public GameObject hudPrefab;
 
     }
 }
